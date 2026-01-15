445687eede732c569635fb87eef4122ab0743789
KIIT <dhrubakunduu@gmail.com>
2026-01-15 23:47:01 +0530

    initial commit

--- src/LAB1/Q5.java
+++ src/LAB1/Q5.java
@@ -1,4 +1,14 @@
 package LAB1;
 
+import java.util.Scanner;
+
 public class Q5 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first name:");
+        String first=sc.nextLine();
+        System.out.print("Enter second name:");
+        String second=sc.nextLine();
+        System.out.println(first+" "+second);
+    }
 }
--- src/LAB2/Q1.java
+++ src/LAB2/Q1.java
@@ -2,17 +2,18 @@ package LAB2;
 import java.util.Scanner;
 public class Q1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int even = 0;
-        int odd = 0;
-        for (int i = 0; i < 10; i++) {
-            int n = sc.nextInt();
-            if (n % 2 == 0)
+        Scanner sc=new Scanner(System.in);
+        int even=0,odd=0;
+        for(int i=0;i<10;i++)
+        {
+            System.out.print("Enter number:");
+            int num=sc.nextInt();
+            if(num%2==0)
                 even++;
             else
                 odd++;
         }
-        System.out.println("Even numbers: " + even);
-        System.out.println("Odd numbers: " + odd);
+        System.out.println("Even:"+even);
+        System.out.print("Odd:"+odd);
     }
 }
